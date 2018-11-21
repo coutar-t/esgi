3353c83676f874608e2a35d56df7851d6a8e74dd
Thibaut Coutard <thibaut.coutard@epitech.eu>
2018-11-21 13:36:04 +0100

    first commit

---
+++ esgi/esgi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  esgi
+//
+//  Created by Thibaut Coutard on 21/11/2018.
+//  Copyright © 2018 Thibaut Coutard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
